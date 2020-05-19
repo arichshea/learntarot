eca6aae4caee7f788b20ed63f04701297b3fd636
arichshea <65359853+arichshea@users.noreply.github.com>
2020-05-19 11:24:14 -0400

    Update user.php
    Added cookie functionality to set default user and level with PHP and then update it with javascript

--- user.php
+++ user.php
@@ -1,8 +1,23 @@
 <?php
 
 function Load_UserArea() {
-	return "<div id='userFrame'><input type='text' id='userName' length='10' />
-			<input type='text' id='userLevel' length='10' /></div>";
+	$user = "briarmoss";
+	$level = 1;
+	if(!isset($_COOKIE["user"])) {
+		setcookie("user",$user, 0, "/");
+		setcookie("level",$level, 0, "/");
+	} else {
+		$user = htmlspecialchars($_COOKIE["user"]);
+		$level = intval($_COOKIE["level"]);
+	}
+	$myCookie = [$user,$level];
+	
+		
+	$userHTML = "<div id='userFrame'><input type='text' id='userName' length='10' value='".$myCookie[0]."' />
+	<input type='text' id='userLevel' length='10' value='".$myCookie[1]."' />
+	<button id='User Update' onclick=\"document.cookie='user='+$('#userName').prop('value')+'; path=/';document.cookie='level='+$('#userLevel').prop('value')+'; path=/';\">Update User</button></div>";
+	
+	return $userHTML;
 	//get user and level from cookie
 	//if no cookie, set to guest, set level to intro
 	//Add register and login options
